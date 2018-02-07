eda714285a0db76ab217479f674a7959097dcbc3
Zoryana Vakh <zoryanavakh@gmail.com>
2018-02-07 17:23:16 +0200

    clear component from unused variables and import, need to find another way of aytocompleting

--- src/app/modules/GoogleDistance/app.component.ts
+++ src/app/modules/GoogleDistance/app.component.ts
@@ -1,24 +1,18 @@
-import { Component, ViewChild, ElementRef, NgZone, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { GoogleDistanceService } from './services/GoogleDistanceService';
-import { } from '@types/googlemaps';
+// import { } from '@types/googlemaps';
 
 @Component({
   selector: 'google-distance',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class GoogleDistanceComponent implements OnInit{
+export class GoogleDistanceComponent{
     loading = false;
     origins = '';
     destinations = '';
-    @ViewChild('search1') public searchElement: ElementRef;
 
-    constructor(
-        private googleApi: GoogleDistanceService,
-        private ngZone: NgZone
-    ) { }
-
-    ngOnInit () {}
+    constructor(private googleApi: GoogleDistanceService) { }
 
     countDistance = () => {
         this.loading = true;
